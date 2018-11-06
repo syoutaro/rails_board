0cbabe0d5cef2c2726f792725244d4d4ec319d1f
sugawara <pgjsugawara258@icloud.com>
2018-11-06 16:19:41 +0900

    add index pages

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'jbuilder',     '2.7.0'
 gem 'annotate'
 gem 'rails-i18n'
 gem 'devise'
+gem 'hamlit-rails'
+gem 'erb2haml'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
---
+++ app/models/board.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  body       :text             not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Board < ApplicationRecord
+end
